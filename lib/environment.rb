c89f70a58c8e75851df3c912047cf8acd17fe876
chrisb3546 <chrisb3546@yahoo.com>
2020-05-29 12:03:50 -0400

    updating environmentand bin

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,6 +3,6 @@ require 'pry'
 require 'httparty' #interacts with API
 require 'json' #parsing data from API into JSON
 
-require_relative "./cli_project/cli"
-require_relative "./cli_project/api"
-require_relative "./cli_project/characters"
+require_relative "./project/api"
+require_relative "./project/character"
+require_relative "./project/cli"
